b6e21b4ccab958741166a60287b2c5bd8160bb79
RyugaXhypeR <ryugashyper@gmail.com>
2025-03-06 13:40:14 +0530

    feat: using getattr trick

--- src/cldig/git_utils.py
+++ src/cldig/git_utils.py
@@ -1,27 +1,24 @@
 import re
-
 import shlex
 
 import git
 
+REVISION_STEPS_RE = re.compile(r"(?P<revisions>\d+) revisions.*(?P<steps>\d+) steps")
+
 
 def first_bad_commit(
     repo: git.Repo, version_good: str, version_bad: str, predicate: str
-) -> tuple[git.Commit, int]:
+) -> tuple[git.Commit, int, int]:
     """Performs a Git bisect to find the first bad commit based on a predicate command."""
 
     predicate_tokens = shlex.split(predicate)
 
-    summary = repo.git._call_process("bisect", "start", version_bad, version_good)
-    repo.git._call_process("bisect", "run", *predicate_tokens)
-    commit = repo.git._call_process("bisect", "view", "--format=%H")
-    repo.git._call_process("bisect", "log").splitlines()
-    repo.git._call_process("bisect", "reset")
+    summary = repo.git.bisect("start", version_bad, version_good)
+    repo.git.bisect("run", *predicate_tokens)
+    commit = repo.git.bisect("view", "--format=%H")
+    repo.git.bisect("reset")
 
-    num_revisions, num_steps = re.findall(r"\d+", summary)[:2]
+    if (match := REVISION_STEPS_RE.search(summary)) is None:
+        raise git.CommandError("Unable to extract number of revisions and steps")
 
-    return repo.commit(commit), num_revisions, num_steps
-
-
-def get_diff(commit: git.Commit) -> git.Diff:
-    return commit.repo.git.diff(f"{commit.hexsha}^!")
+    return repo.commit(commit), int(match["revisions"]), int(match["steps"])
